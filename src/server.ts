f0f754f91f8d166f19ea7e7b83ad29730545eb54
mahyarshaabani <mahyar.shaabani@yahoo.com>
2025-07-11 21:01:06 +0200

    update server port to 3001 and handle startup errors

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import config from './config/config';
 app.listen(config.port, (error) => {
     if (error) {
         console.error(`Error starting server: ${error.message}`);
+        process.exit(1);
     }
     console.log(`Server running on port ${config.port}`);
-});
+})
